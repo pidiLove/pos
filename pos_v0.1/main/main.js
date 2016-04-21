192f85822ab513c7896d613d679e1964091eefa3
suiyuehanhao <1278808836@qq.com>
2016-04-21 12:07:25 +0800

    pos_v0.1

--- pos_v0.1/main/main.js
+++ pos_v0.1/main/main.js
@@ -1,85 +1,76 @@
 function printReceipt(inputs) {
-  var outputs = count(inputs);
-  var items = buildItems(outputs);
-  var cartItems = buildCartItems(items);
-  var receipt = buildReceipt(cartItems);
+  var cartItems = buildCartItems(inputs);
+  var items = buildItems(cartItems);
+  var receipt = buildReceipt(items);
 
   console.log(receipt);
 }
 
-function count(inputs) {
-  var outputs = [];
-  var count = [0,0,0];
-
-  inputs.forEach(function (input) {
-    if (input.barcode == 'ITEM000000')
-      count[0]++;
-    else if (input.barcode == 'ITEM000001')
-      count[1]++;
-    else count[2]++;
-  });
-  var flag1 = 0;
-  var flag2 = 0;
-  var flag3 = 0;
-
-  inputs.forEach(function (input) {
-    if (input.barcode == 'ITEM000000' && flag1 == 0) {
-      outputs.push({barcode: input.barcode, name: input.name, unit: input.unit, price: input.price, count: count[0]});
-      flag1 = 1;
-    }
-    if (input.barcode == 'ITEM000001' && flag2 == 0) {
-      outputs.push({barcode: input.barcode, name: input.name, unit: input.unit, price: input.price, count: count[1]});
-      flag2 = 1;
-    }
-    if (input.barcode == 'ITEM000004' && flag3 == 0) {
-      outputs.push({barcode: input.barcode, name: input.name, unit: input.unit, price: input.price, count: count[2]});
-      flag3 = 1;
-    }
-  });
-
-  return outputs;
-}
-
-function buildItems(outputs) {
-  var items = [];
-
-  outputs.forEach(function (output) {
-    var subtotal = (output.count * output.price);
-
-    items.push({item: output, subtotal: subtotal});
-  });
-
-  return items;
-}
-
-function buildCartItems(items) {
+function buildCartItems(inputs) {
   var cartItems = [];
-  var total = 0;
-
-  items.forEach(function (item) {
-    total += item.subtotal;
+  var flag = 0;
+  inputs.forEach(function (input) {
+    cartItems.forEach(function (cartItem) {
+      if (input.barcode == cartItem.cartItems.barcode)
+        flag = 1;
+    });
+    if (flag == 0)
+      cartItems.push({cartItems: input, count: 0, subtotal: 0});
+    else
+      flag = 0;
   });
-  cartItems = {cartitem: items, total: total};
+  countSameNumber(cartItems, inputs);
 
   return cartItems;
 }
 
-function buildReceipt(cartItems) {
-  return ('***<没钱赚商店>收据***\n' + build(cartItems) + '----------------------\n' +
-  '总计：' + ((cartItems.total).toFixed(2)) + '(元)\n' + '**********************');
+function countSameNumber(cartItems, inputs) {
+
+
+  cartItems.forEach(function (cartItem) {
+    inputs.forEach(function (input) {
+      if (input.barcode == cartItem.cartItems.barcode) {
+        cartItem.count++;
+      }
+
+    });
+    cartItem.subtotal = (cartItem.count * cartItem.cartItems.price);
+
+  });
+
+
+}
+
+function buildItems(cartItems) {
+  var Items = [];
+  var total = 0;
+
+  cartItems.forEach(function (cartItem) {
+    total += cartItem.subtotal;
+  });
+
+  Items = {items: cartItems, total: total};
+
+  return Items;
+}
+
+function buildReceipt(Items) {
+  return ('***<没钱赚商店>收据***\n' + build(Items) + '----------------------\n' +
+  '总计：' + ((Items.total).toFixed(2)) + '(元)\n' + '**********************');
 }
 
 
-function build(cartItems) {
+function build(Items) {
   var text = '';
-
-  var citems = cartItems.cartitem;
-  citems.forEach(function (citem) {
-    text += ('名称：' + citem.item.name + '，' + '数量：' + citem.item.count + citem.item.unit + '，' + '单价：' + (citem.item.price).toFixed(2)
+  (Items.items).forEach(function (citem) {
+    text += ('名称：' + citem.cartItems.name + '，' + '数量：' + citem.count + citem.cartItems.unit + '，' + '单价：' + (citem.cartItems.price).toFixed(2)
     + '(元)' + '，' + '小计：' + (citem.subtotal).toFixed(2) + '(元)\n');
   });
 
   return text;
 }
+
+
+
 
 
