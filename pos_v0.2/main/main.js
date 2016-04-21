c6fd1b9039c2dbcff3a6010e47115779b54bf0de
suiyuehanhao <1278808836@qq.com>
2016-04-21 12:08:05 +0800

    pos_v02

--- pos_v0.2/main/main.js
+++ pos_v0.2/main/main.js
@@ -1,105 +1,76 @@
 function printReceipt(inputs) {
-  var outputs = count(inputs);
-  var items = buildItems(outputs);
-  var cartItems = buildCartItems(items);
-  var receipt = buildReceipt(cartItems);
+  var cartItems = buildCartItems(inputs);
+  var items = buildItems(cartItems);
+  var receipt = buildReceipt(items);
 
   console.log(receipt);
 }
 
-function count(inputs) {
-  var outputs = [];
-  var allItems = [];
-  allItems = loadAllItems();
-  var count = [0, 0, 0];
-
-  inputs.forEach(function (input) {
-    if (input == 'ITEM000000')
-      count[0]++;
-    else if (input == 'ITEM000001')
-      count[1]++;
-    else count[2]++;
-  });
-  var flag1 = 0;
-  var flag2 = 0;
-  var flag3 = 0;
-
-  allItems.forEach(function (allitem) {
-    if (flag1 == 0 && allitem.barcode == 'ITEM000000') {
-      outputs.push({
-        barcode: allitem.barcode,
-        name: allitem.name,
-        unit: allitem.unit,
-        price: allitem.price,
-        count: count[0]
-      });
-      flag1 = 1;
-    }
-    if (flag2 == 0 && allitem.barcode == 'ITEM000001') {
-      outputs.push({
-        barcode: allitem.barcode,
-        name: allitem.name,
-        unit: allitem.unit,
-        price: allitem.price,
-        count: count[1]
-      });
-      flag2 = 1;
-    }
-    if (flag3 == 0 && allitem.barcode == 'ITEM000004') {
-      outputs.push({
-        barcode: allitem.barcode,
-        name: allitem.name,
-        unit: allitem.unit,
-        price: allitem.price,
-        count: count[2]
-      });
-      flag3 = 1;
-    }
-  })
-
-  return outputs;
-}
-
-function buildItems(outputs) {
-  var items = [];
-
-  outputs.forEach(function (output) {
-    var subtotal = (output.count * output.price);
-
-    items.push({item: output, subtotal: subtotal});
-  });
-
-  return items;
-}
-
-function buildCartItems(items) {
+function buildCartItems(inputs) {
   var cartItems = [];
-  var total = 0;
-  items.forEach(function (item) {
-    total += item.subtotal;
+  var flag = 0;
+  var allItems = loadAllItems();
+  allItems.forEach(function (allItem) {
+    inputs.forEach(function (input) {
+      cartItems.forEach(function (cartItem) {
+        if (cartItem.cartItems.barcode == allItem.barcode)
+          flag = 1;
+      });
+      if (input == allItem.barcode && flag == 0)
+        cartItems.push({cartItems: allItem, count: 0, subtotal: 0});
+      else
+        flag = 0;
+    });
+
   });
-  cartItems = {cartitem: items, total: total};
+  countSameNumber(cartItems, inputs);
 
   return cartItems;
 }
 
-function buildReceipt(cartItems) {
-  return ('***<没钱赚商店>收据***\n' + build(cartItems) + '----------------------\n' +
-  '总计：' + ((cartItems.total).toFixed(2)) + '(元)\n' + '**********************');
+function countSameNumber(cartItems, inputs) {
+  cartItems.forEach(function (cartItem) {
+    inputs.forEach(function (input) {
+      if (input == cartItem.cartItems.barcode) {
+        cartItem.count++;
+      }
 
+    });
+    cartItem.subtotal = (cartItem.count * cartItem.cartItems.price);
+
+  });
+}
+
+function buildItems(cartItems) {
+  var Items = [];
+  var total = 0;
+
+  cartItems.forEach(function (cartItem) {
+    total += cartItem.subtotal;
+  });
+
+  Items = {items: cartItems, total: total};
+
+  return Items;
+}
+
+function buildReceipt(Items) {
+  return ('***<没钱赚商店>收据***\n' + build(Items) + '----------------------\n' +
+  '总计：' + ((Items.total).toFixed(2)) + '(元)\n' + '**********************');
 }
 
 
-function build(cartItems) {
+function build(Items) {
   var text = '';
-  var citems = cartItems.cartitem;
-
-  citems.forEach(function (citem) {
-    text += ('名称：' + citem.item.name + '，' + '数量：' + citem.item.count + citem.item.unit + '，' + '单价：' + (citem.item.price).toFixed(2)
+  (Items.items).forEach(function (citem) {
+    text += ('名称：' + citem.cartItems.name + '，' + '数量：' + citem.count + citem.cartItems.unit + '，' + '单价：' + (citem.cartItems.price).toFixed(2)
     + '(元)' + '，' + '小计：' + (citem.subtotal).toFixed(2) + '(元)\n');
   });
 
   return text;
 }
+
+
+
 
 
